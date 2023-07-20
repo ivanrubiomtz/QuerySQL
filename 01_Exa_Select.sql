ffd52ea1cd1a1be943c0c3e71cba0298c42bb90b
Iván <69028617+ivanrubiomtz@users.noreply.github.com>
2023-07-20 13:57:16 -0600

    01_Exa_Select.sql

---
+++ 01_Exa_Select.sql
@@ -0,0 +1,39 @@
+
+--1.	Consulta desde el Analizador de Consultas, de la base de datos Northwind, con las tablas Products y Categories, de los siguientes campos:
+---	ProductID
+---	ProductName
+---	CategoryID
+---	CategoryName
+---	UnitPrice
+
+
+--a.	Filtre únicamente las categorías 1 y 7
+
+USE Northwind
+GO
+SELECT ProductID, ProductName, P.CategoryID, CategoryName, UnitPrice FROM Products P
+	INNER JOIN (
+		    SELECT * FROM Categories
+		    )C ON C.CategoryID = P.CategoryID
+            	    WHERE P.CategoryID IN (1,7)
+
+					
+--b.	Eliminar el filtro anterior y filtre de la categoría 3 a la 6 utilizando BETWEEN.
+
+USE Northwind
+GO
+SELECT ProductID, ProductName, P.CategoryID, CategoryName, UnitPrice FROM Products P
+	INNER JOIN (
+		    SELECT * FROM Categories
+		    )C ON C.CategoryID = P.CategoryID
+			WHERE P.CategoryID BETWEEN 3 AND 6
+
+--c.	Respete el filtro del punto anterior y ordénelo por CategoryID en forma descendente.
+
+USE Northwind
+GO
+SELECT ProductID, ProductName, P.CategoryID, CategoryName, UnitPrice FROM Products P
+	INNER JOIN (
+		    SELECT * FROM Categories
+		    )C ON C.CategoryID = P.CategoryID
+			WHERE P.CategoryID BETWEEN 3 AND 6 ORDER BY P.CategoryID DESC
